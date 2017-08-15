cc90008d518189fdb1610fc178ddb28f0c5fb76a
Ruby Kim <rubyk70231@gmail.com>
2017-08-15 10:50:16 +0200

    add routes and controllers

---
+++ app/controllers/portfolio_trackers_controller.rb
@@ -0,0 +1,7 @@
+class PortfolioTrackersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+end
---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,10 @@
+class PortfoliosController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
