4524e9f256d8ac6cd3f1800a9ac7e6f49260e3a0
Ruby Kim <rubyk70231@gmail.com>
2017-08-15 11:01:38 +0200

    accidental change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/pages/tour', to: 'pages#tour'
+  get '/pages/about', to: 'pages#about'
+  get '/pages/faq', to: 'pages#faq'
+  get '/pages/contact_us', to: 'pages#contact_us'
+
+  resources :investors, only: [:index, :show]
+  resource :account, only: [:show, :edit, :update]
+  resource :portfolio, only: [:show]
+  resources :portfolio_trackers, only: [:create, :new] do
+    resources :operations, only: [:create]
+  end
 end
