e178f025be458ce703ec3724bc6aa66279d271a7
Sandi Huynh <shuynh1300@yahoo.com>
2017-08-17 18:25:51 +0200

    add preview box, cloudinary, carrierwave, inverstor photos, and more styling

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -2,4 +2,6 @@ class Investor < ApplicationRecord
   has_many :investor_stocks, dependent: :destroy
   has_many :stocks, through: :investor_stocks
   has_many :stock_histories, through: :investor_stocks
+
+  mount_uploader :photo, PhotoUploader
 end
