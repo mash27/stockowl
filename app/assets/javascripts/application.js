02ab067a805d566b9786921fc486eb1770fff046
Ash Madi <a.madi27@gmail.com>
2017-08-17 18:51:08 +0200

    Fix issue in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,9 @@
 //   }
 // });
 
- $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });
-
-
-  }
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
 });
 
 
