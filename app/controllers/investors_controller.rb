cec317940b727bef1701337115ea5b514223ac31
Ash Madi <a.madi27@gmail.com>
2017-08-17 16:28:59 +0200

    comment out api code in investor controller

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -6,12 +6,12 @@ class InvestorsController < ApplicationController
   def show
     @investor = Investor.find(params[:id])
     # Build all the current daily series price for all stocks of this investor
-    @stocks = @investor.stocks.map do |stock|
-      daily_price_serie = GetCurrentDailyPriceSeriesForStockService.new(stock.ticker).call
-      # See stock.rb, attr_accessor just to bundle together a stock and its current daily price serie
-      # from the API
-      stock.daily_price_serie = daily_price_serie
-      stock
-    end
+    # @stocks = @investor.stocks.map do |stock|
+    #   daily_price_serie = GetCurrentDailyPriceSeriesForStockService.new(stock.ticker).call
+    #   # See stock.rb, attr_accessor just to bundle together a stock and its current daily price serie
+    #   # from the API
+    #   stock.daily_price_serie = daily_price_serie
+    #   stock
+    # end
   end
 end
