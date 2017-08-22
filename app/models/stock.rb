6a2d96e91f912d571e6efc7773807b50062ef312
Ash Madi <a.madi27@gmail.com>
2017-08-22 10:41:34 +0200

    use delimiter instead of method

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,17 +8,17 @@ class Stock < ApplicationRecord
   attr_accessor :daily_price_serie
 
 
-  def self.transform_number(number)
-   new_array = []
-    array = number.to_i.to_s.split("")
-    array.reverse.each_with_index do |x, i|
-      if ((i + 1) % 3 == 0) && !(i == array.size - 1)
-        new_array << "," + x
-      else
-        new_array << x
-      end
-    end
-    new_array.reverse.join("")
-  end
+  # def self.transform_number(number)
+  #  new_array = []
+  #   array = number.to_i.to_s.split("")
+  #   array.reverse.each_with_index do |x, i|
+  #     if ((i + 1) % 3 == 0) && !(i == array.size - 1)
+  #       new_array << "," + x
+  #     else
+  #       new_array << x
+  #     end
+  #   end
+  #   new_array.reverse.join("")
+  # end
 
 end
