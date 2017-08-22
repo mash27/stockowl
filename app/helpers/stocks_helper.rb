e195a6cf8d3810544ec70f145634b319e7e43cfe
Ash Madi <a.madi27@gmail.com>
2017-08-22 18:25:10 +0200

    add features to show table

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,9 +1,21 @@
 module StocksHelper
   def stock_change_css_class(percentage)
+    return if percentage.nil?
+
     if percentage >= 10
       'stock-increase'
     elsif percentage <= -10
       'stock-decrease'
+    end
+  end
+
+  def holding_change_css_class(number)
+    return if number.nil?
+
+    if number > 0
+      'holding-increase'
+    elsif number < 0
+      'holding-decrease'
     end
   end
 end
